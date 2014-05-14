bbc643981ec84baee150fe0d38fc35b6496d2b45
mseifert <m.seifert@syseleven.de>
2014-05-14 06:43:06 +0200

    Fixed Tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
     throw new \RuntimeException('Install the dependencies to run the test suite.');
@@ -9,4 +10,24 @@ if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
 
 $loader = require $file;
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+AnnotationRegistry::registerLoader('class_exists');
+
+require_once __DIR__.'/Functional/app/AppKernel.php';
+
+// Setting up the fixtures
+$kernel = new \AppKernel("test", true);
+$kernel->boot();
+
+$application = new Application($kernel);
+$application->setAutoExit(false);
+runConsole("doctrine:schema:drop", array("--force" => true), $application);
+runConsole("doctrine:schema:create",array(), $application);
+runConsole("doctrine:fixtures:load", array("--fixtures" => __DIR__ . "/../DataFixtures",'-n' => true), $application);
+
+function runConsole($command, Array $options = array(), Application $application)
+{
+    $options["-e"] = "test";
+    // $options["-q"] = null;
+    $options = array_merge($options, array('command' => $command));
+    return $application->run(new \Symfony\Component\Console\Input\ArrayInput($options));
+}
